cee1f649cf7eaf3e08fb21e1e94a0bd5390a5fd9
rmunson <russell.munson@mlb.com>
2014-07-23 14:40:21 -0400

    adding umd factor, and updating the readme.

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 jquery.simple-touch
 ==========
 
-Ensure .touches are monitored by jQuery touch* listeners.
+Ensure event.touches are monitored by jQuery touch listeners.
 
 ## Basic Usage
+Simple touch can be used as a requirejs module or standard script include.  
+
+If used with requirejs, simple-touch will return the required instance of jquery that has been augmented for convenience.
 
 ```
-    var jquery.simpleTouch = require("jquery.simple-touch/jquery.simple-touch");
-    
+    var jqueryWithTouch = require("jquery.simple-touch/jquery.simple-touch");
+
+    or
+
+    <script src="jquery.simple-touch/jquery.simple-touch.js" /></script>
+
 ```
--- jquery.simple-touch.js
+++ jquery.simple-touch.js
@@ -1,10 +1,21 @@
-;(function(global){
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], function (jQuery) {
+            return factory(jQuery);
+        });
+    } else if (typeof module !== 'undefined' && module.exports) {
+		var jQuery = require('jquery');
+		module.exports = factory( jQuery );
+	} else{
+    	factory(jQuery);
+    }
+}(this,function(jQuery){
 	/**
 	 * Very basic extension to jQuery events to ensure touches are monitored.  Simply for support right now
 	 * but possibly an avenue for further extension at one point. 
 	 * @author rmunson
 	 */
-	var jQuery=global.jQuery;
     if(jQuery && jQuery.event){
     	jQuery.event.touchHooks=$.extend(true,{},jQuery.event.mouseHooks,{props:['touches']});
 
@@ -12,4 +23,4 @@
 			jQuery.event.fixHooks[name] = jQuery.event.touchHooks;
 	    });
     }
-})(this);
+}));
